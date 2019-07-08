8e1e52c7556fca0c12cb1c58909d58a80ee39494
mark247 <takamamonosora.24@gmail.com>
2019-07-08 08:59:49 +0000

    validate

--- app/models/theme.rb
+++ app/models/theme.rb
@@ -1,7 +1,8 @@
 class Theme < ApplicationRecord
   belongs_to :user
-  has_many :comments
+  has_many :comments, dependent: :destroy
   
   validates :content, presence: true, length: { maximum: 20 }
   validates_uniqueness_of:content
+  
 end
