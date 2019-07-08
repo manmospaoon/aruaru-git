c89c661f84edc129e441bb173b540d10cecc9eab
mark247 <takamamonosora.24@gmail.com>
2019-07-08 10:04:47 +0000

    scope

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ApplicationRecord
   validates :content, presence: true, length: { maximum: 50 }
-  validates_uniqueness_of:content
+  validates_uniqueness_of:content, :scope => :theme_id
   
   belongs_to :user
   belongs_to :theme
